2cb10f4eae95d48f05e8e10d9b76b1c57effe4e4
Anirudh Aggarwal <starfire1797@gmail.com>
2017-03-14 12:55:56 -0400

    Removed all Merge Errors. Please make sure you work only on your files and request the changes on other files.

--- Hotel-Project/src/java/com/hotelProject/controllers/Admin.java
+++ Hotel-Project/src/java/com/hotelProject/controllers/Admin.java
@@ -6,8 +6,11 @@
 package com.hotelProject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import wrappers.Room;
 
 /**
  *
@@ -20,5 +23,28 @@ public class Admin {
     public ModelAndView AdminController(ModelAndView mv){
         mv = new ModelAndView("admin");
         return mv;
+    }
+    public ModelAndView addRooms(@ModelAttribute("room") Room room)
+    {
+        // add room to database please make sure the data is correct and redirect accordingly
+        return null;
+    }
+    
+    public ModelAndView editRooms(@ModelAttribute("room") Room room)
+    {
+        // add room to database please make sure the data is correct and redirect accordingly
+        return null;
+    }
+   
+    public ModelAndView deleteRooms(@RequestParam("roomNumber") int id)
+    {
+        // add room to database please make sure the data is correct and redirect accordingly
+        return null;
+    }
+    
+    public ModelAndView getReservation(@ModelAttribute("newWrapper") String data)
+    {
+        // get the information and store it in the wrapper class and finally give the right result based on the search!
+        return null;
     }
 }
